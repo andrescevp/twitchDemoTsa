fb8890c7d9a5b96378ddf8031ad4c7d040681883
Andres Cevallos <andrescevp@gmail.com>
2020-03-18 00:30:04 +0100

    encore y template base

---
+++ src/Command/ShowPhpStatusCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ShowPhpStatusCommand extends Command
+{
+    protected static $defaultName = 'app:show-php-status';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->note(phpinfo());
+
+        return 0;
+    }
+}
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,12 +3,24 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function indexAction()
     {
+        $this->logger->critical('Esto es un error muy importante');
         return $this->render('app/index.html.twig');
     }
 }
